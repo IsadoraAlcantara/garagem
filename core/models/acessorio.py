23a244fc0e7f65302ad2c4b5d639034b49a12256
IsadoraAlcantara <isadoraalca405@gmail.com>
2024-03-22 09:31:45 -0300

    criação da api acessorio

--- core/models/acessorio.py
+++ core/models/acessorio.py
@@ -4,5 +4,5 @@ from django.db import models
 class Acessorio(models.Model):
     descricao = models.CharField(max_length=100)
 
-    def __src__(self):
+    def __str__(self):
         return f"{self.descricao} ({self.id})"
